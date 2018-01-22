98555faed921891c60e1e573f96191b975973792
badongsch <badongsch@gmail.com>
2018-01-22 17:57:02 +0900

    숫자를 입력 받아 구구단을 만든다.

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Gugudan {
 
@@ -8,14 +9,21 @@ public class Gugudan {
 //		}
 		
 		// 1단 ~ 10딘
-		int i = 0;
-		int j = 0;	
+		int i = 1;
+		//int j = 0;	
+		System.out.println( " *********단을 입력해 주세요. ********* " );
+		Scanner scanner = new Scanner(System.in);		
+		int j  = scanner.nextInt();
+//				for	(j=1 ; j <=10; j++) {
+//			System.out.println( " *********"+ j +"단 입니다. ********* " );
+//			for(int i=1 ; i<=10 ; i++) {
+//				System.out.println( j + " * " + i +" = " + j * i );	
+//			}
+//		}
 		
-		for	(j=1 ; j <=10; j++) {
-			System.out.println( " *********"+ j +"단 입니다. ********* " );
-			for(i=1 ; i<=10 ; i++) {
-				System.out.println( j + " * " + i +" = " + j * i );	
-			}
+		while(i<=10) {
+			System.out.println( j + " * " + i +" = " + j * i );
+			i++;
 		}
 	}
 }
