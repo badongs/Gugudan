3771c45d00e41ffea6500389b5ddfb6ccb3034e0
badongsch <badongsch@gmail.com>
2018-01-23 09:59:39 +0900

    구구단 메소드 만들기.

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,29 +1,57 @@
 import java.util.Scanner;
 
 public class Gugudan {
-
+	public static void caculate(int times) {
+		int[] rs = new int[10];
+		System.out.println( " ********* " + times + "단을 입력해 주세요. ********* " );
+		for(int i=0 ; i<rs.length ; i++) {			
+			System.out.println( times + " * " + (i+1) +" = " + times * (i+1) );
+		}
+				
+	}
+	
 	public static void main(String[] args) {
-		// 2단
-//		for(int i=1 ; i<=10 ; i++) {
-//			System.out.println( "2 *"+ i +" = " +2*i );	
-//		}
+		
+		caculate(1) ;
+		for(int i=1 ; i<=10 ; i++) {
+			caculate(i) ;	
+		}
 		
 		// 1단 ~ 10딘
-		int i = 1;
-		//int j = 0;	
-		System.out.println( " *********단을 입력해 주세요. ********* " );
-		Scanner scanner = new Scanner(System.in);		
-		int j  = scanner.nextInt();
-//				for	(j=1 ; j <=10; j++) {
-//			System.out.println( " *********"+ j +"단 입니다. ********* " );
-//			for(int i=1 ; i<=10 ; i++) {
-//				System.out.println( j + " * " + i +" = " + j * i );	
+//		int i = 1;
+//		System.out.println( " *********단을 입력해 주세요. ********* " );
+//		Scanner scanner = new Scanner(System.in);		
+//		int j  = scanner.nextInt();
+//		
+//		while(i<=10) {
+//			System.out.println( j + " * " + i +" = " + j * i );
+//			i++;
+//		}
+//		
+//		for(i=1 ; i<=10 ; i++) {
+//			System.out.println( j + " * " + i +" = " + j * i );	
+//		}
+		
+//		배열을 이용한 구구단
+	
+//		System.out.println( " *********단을 입력해 주세요. ********* " );
+//		Scanner scanner = new Scanner(System.in);		
+//		int j  = scanner.nextInt();
+//		
+//		int[][] result = new int[9][10];
+//		for(int i=0; i < result.length; i++) {
+//			
+//			for(int j=0; j < result[i].length; j++) {
+//				result[i][j] = (i+1) *(j+1);				
+//			}
+//		}
+//		
+//		for(int i=0; i < result.length; i++) {
+//			System.out.println( " ********* " + i + "단을 입력해 주세요. ********* " );
+//			for(int j=0; j < result[i].length; j++) {
+//				System.out.println(result[i][j]);
 //			}
 //		}
 		
-		while(i<=10) {
-			System.out.println( j + " * " + i +" = " + j * i );
-			i++;
-		}
 	}
 }
